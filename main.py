554660c44af87c7900ae56e9b5bfd268d2d33b78
Sergei Khakimyanov <s.khakimyanov@bk.ru>
2022-08-26 13:39:13 +0500

    Добавлен модуль text.py, добавлена функция multi, удаление текста из main.py, добавление функции up() в text.py
    Функция multi:
    -Умножение двух переменных ( а и b)
    -Находится в main.py
    
    Файл text.py:
    -Теперь весь текст из проекта подается сюда
    -Все функции связанные с форматированием и редактированием текста, создавать здесь же
    -Добавлена функция преобразования текста в верхний регистр up()

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def plus(a,b):
     return a + b
 print(plus(4,6))
 
-text = "Жопа слона"
+
 def minus(a,b):
     return a - b
 print(minus(7,8))
+
+def multi(a,b):
+    return a * b
+print(multi(5,6))
 
