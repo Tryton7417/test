38f360622dbd24b8d9858329e8006844d7d611da
Sergei Khakimyanov <s.khakimyanov@bk.ru>
2022-08-27 13:58:59 +0500

    print() перенесен в тело функций, изменено имя модуля.

--- stupid_funct.py
+++ stupid_funct.py
@@ -1,13 +1,15 @@
 def plus(a,b):
     return a + b
-print(plus(4,6))
+    print(plus(4, 6))
+
 
 
 def minus(a,b):
     return a - b
-print(minus(7,8))
+    print(minus(7, 8))
+
 
 def multi(a,b):
     return a * b
-print(multi(5,6))
+    print(multi(5,6))
 
